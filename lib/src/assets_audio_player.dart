ae14876f6d70d064d1a9d83617201e50d9ff9388
chetanchadalavada <@Romans614>
2023-07-07 11:42:21 -0600

    do not stop the player after audio is finished.

--- lib/src/assets_audio_player.dart
+++ lib/src/assets_audio_player.dart
@@ -956,6 +956,8 @@ class AssetsAudioPlayer {
     if (!_playlistAutoPlay) {
       // stop continuous playing when autoplay is off
       _playlistFinished.add(true);
+      await pause();
+      await seek(Duration.zero, force: true);
       return;
     }
     final nextDone = await _next(stopIfLast: false, requestByUser: false);
